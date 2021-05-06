4e698144a82e004072317f20cfe9b8cb921f2a7e
Eun Jin <ouhouh2222@gmail.com>
2021-05-06 22:06:23 +0900

    CURD

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Board CRUD Project
+
+<pre>
+    Board System develop width express, mongoDB, pug
+</pre>
+
+- [ ] BOARD LIST("/)
+- [ ] BOARD DETAIL("/detail/id=${id}")
+- [ ] BOARD CREATE("/create")
+- [ ] BOARD EDIT("/edit/id=${id")
---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import boardRouter from "./routers/boardRouter";
+
+const app = express();
+const PORT = 3000;
+
+app.use(helmet());
+app.use(morgan(`dev`));
+app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "/assets")));
+app.use("/", boardRouter);
+
+app.listen(PORT, () => {
+  console.log(`${PORT} Server Start`);
+});
